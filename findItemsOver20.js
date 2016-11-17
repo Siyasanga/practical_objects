d995999e6dcc6eb85b86feb9076b0d2099244c95
Siyasanga <siyasanga@live.co.za>
2016-11-17 11:40:55 +0200

    Just finished this 3rd exercise on objects

---
+++ findItemsOver20.js
@@ -0,0 +1,20 @@
+// findItemsOver20() takes a list of objects, each object having 2 attributes
+// one called name and the other price.
+// It returns all the products that have a price more than R20.
+function findItemsOver20(list){
+  var expensive = [];
+  for(i in list){
+    if(list[i].price > 20){
+      expensive.push(list[i]);
+    }
+  }
+  return expensive;
+}
+var shoppingList = [
+    { itemName : 'Apples', price : 7.50 },
+    { itemName : 'Bread', price : 24.00 },
+    { itemName : 'Milk', price : 7.00 },
+    { itemName : 'Cheese', price : 23.00 },
+    { itemName : 'Melrose', price : 27.50 }
+];
+console.log(findItemsOver20(shoppingList));;
