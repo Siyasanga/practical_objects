6f6f0695777101ccd0a6d412f823f8719a5df8e0
Siyasanga <siyasanga@live.co.za>
2016-11-23 14:16:41 +0200

    return the department with the most sum of sales

--- mostProfitableDepartment.js
+++ mostProfitableDepartment.js
@@ -1,15 +1,28 @@
 // determine which of their departments are the most profitable
-// Returns object madDep, which is a department with the most sales
+// Returns a string with the department that has the most sales
 function mostProfitableDepartment(salesData){
-  var maxSales = 0;
-  var maxDep;
+  var maxDep = 0;
+  var depSales = {};
+  var department;
+  // Calculating totals with for-loop
   for (i in salesData){
-    if(salesData[i].sales > maxSales){
-      maxSales = salesData[i].sales;
-      maxDep = salesData[i];
+    if(depSales[salesData[i].department] == undefined){
+      depSales[salesData[i].department] = salesData[i].sales;
+    }
+    else {
+      depSales[salesData[i].department] = depSales[salesData[i].department] + salesData[i].sales;
     }
   } // end of for-loop
-  return maxDep;
+  // Looking for the most profitable department
+  for(i in depSales){
+    if(i == undefined)
+      break;
+    if(depSales[i] > maxDep){
+      maxDep = depSales[i];
+      department = i;
+    }
+  }
+  return (department+": R"+depSales[department]);
 } // End of mostProfitableDepartment
 var salesData = [
     {department : 'hardware', sales : 4500, day : 'Monday'},
@@ -29,7 +42,8 @@ var salesData = [
     {department : 'carpentry', sales : 16109, day : 'Friday'},
 ];
 const assert = require('assert');
+console.log(mostProfitableDepartment(salesData));
 // Next we get the most profitable department.
-assert.equal(mostProfitableDepartment(salesData)['department'],'outdoor');
+// assert.equal(mostProfitableDepartment(salesData)['department'],'outdoor');
 // Next we get the most profitable day.
-assert.equal(mostProfitableDepartment(salesData)['day'],'Thursday');
+// assert.equal(mostProfitableDepartment(salesData)['day'],'Thursday');
